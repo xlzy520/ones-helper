e7531c55597c58c0de2276f07f8f243ee73dc478
xlzy <1017866168@qq.com>
2021-12-08 18:33:52 +0800

    feat: 优化更新体验

--- src/popup/features/otherAction/index.vue
+++ src/popup/features/otherAction/index.vue
@@ -14,7 +14,7 @@
         </n-tooltip>
       </div>
       <n-alert title="温馨提示" type="info">
-        <div>下载新版本后，需要删除之前的文件夹，然后将新的文件夹<b style="color:#db2777">重命名</b></div>
+        <div>下载新版本后，需要<b style="color:#db2777">删除之前的文件夹</b>，然后将新的文件夹<b style="color:#db2777">重命名</b>, 也就是删除括号及括号里面的内容</div>
         <!--        <div>构建系统注入到项目一些配置项，提供编辑用于快速调试（不会保存到项目）！</div>-->
         <!--        <div>下面<b style="color:#22c55e">配置项修改后生效</b>需要点击<b style="color:#db2777">保存</b>按钮</div>-->
       </n-alert>
@@ -25,8 +25,8 @@
             {{ currentVersion }}
           </n-tag>
         </div>
-        <n-button v-if="hasNewVersion" type="primary" @click="upgrade">
-          有新版本，立即更新插件
+        <n-button v-if="newVersion" type="primary" :loading="downloading" @click="upgrade">
+          {{ downloadBtnText }}
         </n-button>
       </div>
     </div>
@@ -48,8 +48,13 @@ import QuestionIcon from '~/components/question-icon.vue'
 
 const message = useMessage()
 
-const hasNewVersion = ref(false)
+const newVersion = ref('')
 const currentVersion = ref('')
+const downloading = ref(false)
+
+const downloadBtnText = computed(() => {
+  return downloading.value ? '下载中...' : '有新版本，立即更新插件'
+})
 
 const getCurrentVersion = () => {
   if (browser.app) {
@@ -60,22 +65,19 @@ const getCurrentVersion = () => {
 }
 
 const getLatestRelease = () => {
-  fetch('https://api.github.com/repos/xlzy520/bilibili-img-uploader/releases/latest').then(res => res.json()).then((res) => {
+  fetch('https://api.github.com/repos/xlzy520/ones-helper/releases/latest').then(res => res.json()).then((res) => {
     console.log(res)
     const latest = res.name
     if (latest !== currentVersion.value) {
-      hasNewVersion.value = true
+      newVersion.value = latest
     }
   })
 }
-//
-// const hasNewVersion = () => {
-//
-// }
 
 const upgrade = () => {
+  downloading.value = true
   browser.downloads.download({
-    url: 'https://github.com/xlzy520/bilibili-img-uploader/releases/download/v1.1.12/bilibili.zip',
+    url: `https://github.com/xlzy520/ones-helper/releases/download/${newVersion.value}/extension.zip`,
     // url: 'https://dl2.ones.ai/FsnPOlRBkkNI1JmqaLRSpXMhrGTI?e=1638949019&token=b0yYnz4avmO3oveG34Z3pTIjRLG52PUcxxogPYkP:Z2XZBlX3zzu8aOjRP_sUE6be6ZY',
     // filename: 'sss/extension.zip',
     // saveAs: true,
@@ -84,13 +86,15 @@ const upgrade = () => {
       console.log(id, state, exists)
       if (id === res) {
         if (state && state.current === 'complete') {
+          downloading.value = false
+          newVersion.value = ''
+          message.success('最新版已下载并解压')
           browser.downloads.open(id)
         }
       }
     })
     console.log(res)
   })
-  message.info('开发中...')
 }
 
 const taskConfig = reactive({
