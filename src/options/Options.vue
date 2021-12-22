2593f6287555fa3e6a1a0cc7120a0e978a713741
林群贺 <linqunhe@ones.ai>
2021-12-22 15:38:57 +0800

    feat: options配置采用tab隔离

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,12 +1,24 @@
 <template>
   <n-message-provider>
-    <custom-api />
-    <features-config />
+    <n-tabs size="large" :default-value="defaultTab" type="card">
+      <n-tab-pane v-for="item in settingOptions" :key="item.name" :name="item.name" :tab="item.name">
+        <component :is="item.component"></component>
+      </n-tab-pane>
+    </n-tabs>
   </n-message-provider>
 </template>
 
 <script setup lang="ts">
-import { NMessageProvider } from 'naive-ui'
+import { NMessageProvider, NTabs, NTabPane } from 'naive-ui'
 import CustomApi from '~/options/custom_api/index.vue'
 import FeaturesConfig from '~/options/features_config/index.vue'
+
+const settingOptions = reactive([{
+  name: 'API转发',
+  component: CustomApi,
+}, {
+  name: '功能开关',
+  component: FeaturesConfig,
+}])
+const defaultTab = ref('API转发')
 </script>
