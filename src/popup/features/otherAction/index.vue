0171edf4b3c0c4bc8dbbb076ac6fd08c29ab967c
林群贺 <linqunhe@ones.ai>
2022-01-05 14:21:01 +0800

    feat: 部分细节及交互的优化

--- src/popup/features/otherAction/index.vue
+++ src/popup/features/otherAction/index.vue
@@ -39,104 +39,108 @@
         优化preview分支选择，由于preview搜索到的分支很多，不好直接选择
       </n-tooltip>
     </div>
-    <div class="py-2">
-      <div class="layout-items-center">
-        <div class="font-bold">
-          一键创建分支
+    <div class="p-2" :style="needGithubTokenStyle">
+      <n-alert v-if="!code" title="请点击上方Github授权" type="default">
+        <p>该区域功能依赖Github Token授权信息，授权后即可使用！</p>
+      </n-alert>
+      <div class="py-2 ">
+        <div class="layout-items-center">
+          <div class="font-bold">
+            一键创建分支
+          </div>
+          <n-tag type="info" class="ml-2">
+            前端版
+          </n-tag>
         </div>
-        <n-tag type="info" class="ml-2">
-          前端版
-        </n-tag>
+        <div class="">
+          <div class="py-1">
+            选择要创建分支的项目(基于主分支)
+          </div>
+          <n-checkbox-group v-model:value="checkedProjects">
+            <n-checkbox
+              v-for="project in projectList"
+              :key="project.repo"
+              checked
+              :value="project.repo"
+              :label="project.repo"
+              class="mr-2"
+            ></n-checkbox>
+          </n-checkbox-group>
+        </div>
+        <div class="">
+          <div class="py-1">
+            输入要创建分支的名称
+          </div>
+          <div class="layout-slide">
+            <n-input
+              v-model:value="branchName"
+              placeholder="分支名称"
+              class="mr-4"
+              clearable
+              @clear="clearBranchName"
+            />
+            <n-button type="primary" ghost :disabled="!canCreateBranches" @click="create">
+              👏 确定
+            </n-button>
+            <n-modal
+              v-model:show="showModal"
+              preset="dialog"
+              title="创建分支"
+              positive-text="确认"
+              @positive-click="createBranches"
+            >
+              确定要在以下项目：
+              <div class="">
+                <div v-for="item in checkedProjects" :key="item" class="py-1 font-bold">
+                  {{ item }}
+                </div>
+              </div>
+
+              中创建 <n-tag type="primary">
+                {{ branchName }}
+              </n-tag> 分支吗？
+            <!--            <n-input v-model:value="newPreset.name" placeholder="输入新的预设名" />-->
+            </n-modal>
+          </div>
+        </div>
       </div>
-      <div class="">
-        <div class="py-1">
-          选择要创建分支的项目(基于主分支)
+      <div class="py-2">
+        <div class="layout-items-center">
+          <div class="font-bold">
+            一键获取项目分支Commit Hash值
+          </div>
+          <n-tag type="info" class="ml-2">
+            前端版
+          </n-tag>
         </div>
-        <n-checkbox-group v-model:value="checkedProjects">
-          <n-checkbox
+        <div class="">
+          <div class="py-1">
+            默认项目列表
+          </div>
+          <n-tag
             v-for="project in projectList"
             :key="project.repo"
-            checked
-            :value="project.repo"
-            :label="project.repo"
             class="mr-2"
-          ></n-checkbox>
-        </n-checkbox-group>
-      </div>
-      <div class="">
-        <div class="py-1">
-          输入要创建分支的名称
+          >
+            {{ project.repo }}
+          </n-tag>
         </div>
         <div class="layout-slide">
-          <n-input
-            v-model:value="branchName"
-            placeholder="分支名称"
-            class="mr-4"
-            clearable
-            @clear="clearBranchName"
-          />
-          <n-button type="primary" ghost :disabled="!canCreateBranches" @click="create">
-            👏 确定
-          </n-button>
-          <n-modal
-            v-model:show="showModal"
-            preset="dialog"
-            title="创建分支"
-            positive-text="确认"
-            @positive-click="createBranches"
-          >
-            确定要在以下项目：
-            <div class="">
-              <div v-for="item in checkedProjects" :key="item" class="py-1 font-bold">
-                {{ item }}
-              </div>
+          <div class="layout-items-center">
+            <div class="py-1">
+              选择查询分支：
             </div>
-
-            中创建 <n-tag type="primary">
-              {{ branchName }}
-            </n-tag> 分支吗？
-            <!--            <n-input v-model:value="newPreset.name" placeholder="输入新的预设名" />-->
-          </n-modal>
-        </div>
-      </div>
-    </div>
-    <div class="py-2">
-      <div class="layout-items-center">
-        <div class="font-bold">
-          一键获取项目Hash值
-        </div>
-        <n-tag type="info" class="ml-2">
-          前端版
-        </n-tag>
-      </div>
-      <div class="">
-        <div class="py-1">
-          默认项目列表
-        </div>
-        <n-tag
-          v-for="project in projectList"
-          :key="project.repo"
-          class="mr-2"
-        >
-          {{ project.repo }}
-        </n-tag>
-      </div>
-      <div class="layout-slide">
-        <div class="layout-items-center">
-          <div class="py-1">
-            选择查询分支：
-          </div>
-          <n-radio-group v-model:value="commitHash.branch" name="radiogroup">
-            <n-space>
-              <n-radio
-                v-for="branch in commitHash.branchOptions"
-                :key="branch.value"
-                :value="branch.value"
-              >
-                {{ branch.label }}
-              </n-radio>
-            </n-space>
-          </n-radio-group>
+            <n-radio-group v-model:value="commitHash.branch" name="radiogroup">
+              <n-space>
+                <n-radio
+                  v-for="branch in commitHash.branchOptions"
+                  :key="branch.value"
+                  :value="branch.value"
+                >
+                  {{ branch.label }}
+                </n-radio>
+              </n-space>
+            </n-radio-group>
           <!--          <n-select-->
           <!--            v-model:value="commitHash.branch"-->
           <!--            class="w-[200px]"-->
@@ -144,10 +148,11 @@
           <!--            clearable-->
           <!--            :options="commitHash.branchOptions"-->
           <!--          />-->
+          </div>
+          <n-button type="primary" ghost :loading="copyHashLoading" :disabled="copyHashLoading" @click="getAllCommitHashAndCopy">
+            ✨ 复制全部Hash
+          </n-button>
         </div>
-        <n-button type="primary" ghost @click="getAllCommitHashAndCopy">
-          ✨ 复制全部Hash
-        </n-button>
       </div>
     </div>
     <jenkins />
@@ -155,7 +160,7 @@
 </template>
 
 <script setup lang="ts">
-import { useMessage } from 'naive-ui'
+import { useMessage, NTooltip, NAlert } from 'naive-ui'
 import { onesConfigService } from '~/service'
 import QuestionIcon from '~/components/question-icon.vue'
 import { createNewBranch, fetchBranchSHA, getGithubOAuthToken } from '~/service/github'
@@ -174,6 +179,10 @@ const otherConfig = reactive({
 const code = ref('')
 const privateCode = computed(() => {
   return code.value.substring(0, 16)
+})
+
+const needGithubTokenStyle = computed(() => {
+  return !code.value ? { 'pointer-events': 'none', 'background-color': 'rgba(0,0,0,0.05)', 'cursor': 'not-allowed', 'user-select': 'none' } : {}
 })
 
 const checkedProjects = ref(projectList.map(v => v.repo))
@@ -219,7 +228,10 @@ const commitHash = reactive({
     { label: 'master', value: 'master' },
   ],
 })
+
+const copyHashLoading = ref(false)
 const getAllCommitHashAndCopy = () => {
+  copyHashLoading.value = true
   const apis = projectList.map((project) => {
     return fetchBranchSHA({ ...project, head: commitHash.branch })
   })
@@ -232,6 +244,11 @@ const getAllCommitHashAndCopy = () => {
     })
     copyToClipboard(text.join('\r\n'))
     message.success('复制成功')
+    copyHashLoading.value = false
+  }).catch((err) => {
+    if (err) {
+      copyHashLoading.value = false
+    }
   })
 }
 
