2545b30c78e1a16b0b3a44d53cf602e5079fa1c6
xlzy <1017866168@qq.com>
2022-01-05 19:05:14 +0800

    feat: 优化功能列表长度变化时合并老配置

--- src/service/featuresConfig.ts
+++ src/service/featuresConfig.ts
@@ -2,6 +2,9 @@ import { featuresList } from '~/common/constants'
 
 export function getFeaturesConfig(): Promise<any> {
   return browser.storage.local.get('featuresConfig').then((res: any) => {
+    if (res.featuresConfig.length !== featuresList.length) {
+      return Object.assign(featuresList, res.featuresConfig)
+    }
     return res.featuresConfig || featuresList
   })
 }
