0865924ca1b5c620c4419a76c1a4c18333730a6d
xlzy <1017866168@qq.com>
2022-01-06 14:09:10 +0800

    feat: Jenkins增加Scan功能

--- src/popup/features/jenkins/index.vue
+++ src/popup/features/jenkins/index.vue
@@ -55,28 +55,39 @@
       <div class="layout-slide">
         <div class="layout-items-center">
           <div class="font-bold">
-            Jenkins立即触发构建(dev)
+            Jenkins立刻 Scan 仓库(dev)
           </div>
           <n-tag type="info" class="ml-2">
             前端版
           </n-tag>
-          <n-tooltip trigger="hover">
-            <template #trigger>
-              <question-icon class="ml-2" />
-            </template>
-            场景：本次改动只修改了common，没有自动触发构建
-          </n-tooltip>
         </div>
-        <n-button type="success" ghost :disabled="!jenkinsCrumb" @click="scan">
+      </div>
+      <div class="layout-slide">
+        <n-checkbox-group v-model:value="checkedProjects">
+          <n-checkbox
+            v-for="project in mainProjects"
+            :key="project"
+            checked
+            :value="project"
+            :label="project"
+            class="mr-2"
+          ></n-checkbox>
+        </n-checkbox-group>
+        <n-button type="success" ghost :disabled="!checkedProjects.length" @click="scan">
           🔍️ 立刻 Scan 仓库
         </n-button>
       </div>
     </div>
+
+    <n-button disabled @click="privateDeploy">
+      一键私有部署(WIP)
+    </n-button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useMessage } from 'naive-ui'
+import Browser from 'webextension-polyfill'
 import QuestionIcon from '~/components/question-icon.vue'
 import { projectList } from '~/common/constants'
 
@@ -110,6 +121,14 @@ const ErrorCodeMapping = {
 
 const needGetToken = ref(false)
 
+const currentAction = ref(null)
+
+const privateDeploy = () => {
+  Browser.runtime.sendMessage({
+    type: 'privateDeploy',
+  })
+}
+
 const fetchBuild = (repo: string) => {
   const branchName = JenkinsBuildBranch.value
   return fetch(`https://cd.myones.net/job/development/job/${repo}/job/${branchName}/build?delay=0sec`, {
@@ -137,6 +156,32 @@ const fetchBuild = (repo: string) => {
   })
 }
 
+const fetchScan = (repo: string) => {
+  return fetch(`https://cd.myones.net/job/development/job/${repo}/build?delay=0sec`, {
+    method: 'POST',
+    headers: {
+      'jenkins-crumb': jenkinsCrumb.value,
+    },
+  }).then((res) => {
+    const status = res.status
+    let errorMessage = ErrorCodeMapping[res.status]
+    if (status === 404) {
+      errorMessage = `项目${repo}${errorMessage}`
+    }
+    if (status === 403) {
+      needGetToken.value = true
+    }
+    if (errorMessage) {
+      message.error(errorMessage)
+      return Promise.reject(new Error(errorMessage))
+    }
+    else {
+      message.success(`项目${repo} Scan成功`)
+      return Promise.resolve()
+    }
+  })
+}
+
 const dispatchBuild = () => {
   const apis = checkedProjects.value.map((project) => {
     return fetchBuild(project)
@@ -149,7 +194,14 @@ const dispatchBuild = () => {
 }
 
 const scan = () => {
-  console.log(222)
+  const apis = checkedProjects.value.map((project) => {
+    return fetchScan(project)
+  })
+  Promise.all(apis).catch(() => {
+    if (needGetToken.value) {
+      getLatestJenkinsCrumb()
+    }
+  })
 }
 
 onMounted(() => {
@@ -160,8 +212,7 @@ onMounted(() => {
     if (type === 'jenkins-crumb') {
       jenkinsCrumb.value = data
       browser.storage.local.set({ jenkinsCrumb: data })
-      message.success('Jenkins凭证获取成功, 正在重新执行触发构建')
-      dispatchBuild()
+      message.success('Jenkins凭证获取成功, 请重新执行')
     }
   })
 })
