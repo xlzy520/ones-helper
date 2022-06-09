24f27b7f4c6bde6b4f5c9afce7ac136951756aae
xlzy <1017866168@qq.com>
2022-06-09 15:36:53 +0800

    refactor: 迁移api转发

--- src/background/custom_api/index.ts
+++ src/background/custom_api/index.ts
@@ -1,55 +1,8 @@
-import { HeaderCustomer, HeaderCustomerOptions, Headers } from '../utils/header_customer';
+import { HeaderCustomer } from '../utils/header_customer';
 import { customApiService } from '../../service';
-import {
-  PROJECT_BRANCH_KEY,
-  ONES_HOST_KEY,
-  DefaultPresetOptions,
-  DefaultPreset,
-} from '~/common/constants';
-import { PatternConfig, PresetOption } from '~/service/custom_api';
+import { PatternConfig } from '~/service/custom_api';
 
 const headerCustomer = new HeaderCustomer();
-
-function syncApiSetting(headerCustomer: HeaderCustomer) {
-  browser.storage.local.get('customApiData').then(({ customApiData = {} as any }) => {
-    const headerBuilder: HeaderCustomerOptions['headersBuilder'] = (details) => {
-      const headers: Headers = [];
-      // 兼容火狐，第一次拿到customApiData的时候是undefined
-      const { preset = DefaultPreset, presetOptions = DefaultPresetOptions as PresetOption[] } =
-        customApiData;
-      const selectedConfig = presetOptions.find((v: any) => v.value === preset).config;
-      const customHOST = selectedConfig[ONES_HOST_KEY];
-      const isProjectApi = details.url.includes('/api/project');
-      // console.log(details)
-      if (customHOST) {
-        let value = customHOST;
-        if (!isProjectApi) {
-          value = customHOST.replace('api/project/', 'api/wiki/');
-        }
-        headers.push({
-          name: 'x-ones-api-host',
-          value,
-        });
-      } else {
-        const projectBranch = selectedConfig[PROJECT_BRANCH_KEY];
-        if (projectBranch) {
-          const prefix = isProjectApi ? '/project' : '/wiki';
-          const value = `${prefix}/${projectBranch}/`;
-          headers.push({
-            name: 'x-ones-api-branch-project',
-            value,
-          });
-          headers.push({
-            name: 'x-ones-api-branch-wiki',
-            value,
-          });
-        }
-      }
-      return headers;
-    };
-    headerCustomer.setHeadersBuilder(headerBuilder);
-  });
-}
 
 async function syncPatterns(headerCustomer: HeaderCustomer) {
   const customApiData = await customApiService.getCustomApi();
@@ -65,6 +18,5 @@ async function syncPatterns(headerCustomer: HeaderCustomer) {
 }
 
 export function customApi(): void {
-  // syncApiSetting(headerCustomer);
   syncPatterns(headerCustomer);
 }
--- src/background/utils/header_customer.ts
+++ src/background/utils/header_customer.ts
@@ -12,7 +12,22 @@ import {
 } from '~/common/constants';
 import { PresetOption } from '~/service/custom_api';
 
+const RuleActionType = chrome.declarativeNetRequest.RuleActionType;
+const HeaderOperation = chrome.declarativeNetRequest.HeaderOperation;
+const ResourceType = chrome.declarativeNetRequest.ResourceType;
+
 export type Headers = Browser.WebRequest.HttpHeaders;
+
+export enum ApiPath {
+  Project = '/api/project',
+  Wiki = '/api/wiki',
+}
+
+export interface ProxyParams {
+  id: number;
+  value: ApiPath;
+  urlFilter: string;
+}
 
 export interface HeaderCustomerOptions {
   headersBuilder: (details: Browser.WebRequest.OnBeforeSendHeadersDetailsType) => Headers;
@@ -25,6 +40,17 @@ const getAccessToken = (str = '') => {
   }
   return '';
 };
+
+const resourceTypes = [
+  ResourceType.MAIN_FRAME,
+  ResourceType.STYLESHEET,
+  ResourceType.SCRIPT,
+  ResourceType.FONT,
+  ResourceType.XMLHTTPREQUEST,
+  ResourceType.WEBSOCKET,
+];
+
+const myDomains = ['localhost', 'dev.myones.net', 'ones.ai'];
 
 const setGithubAccessToken = useDebounceFn((code) => {
   const getAccessTokenUrl = `https://github.com/login/oauth/access_token?client_id=${GithubOAuthClientID}&client_secret=${GithubOAuthClientSecrets}&code=`;
@@ -40,7 +66,7 @@ const setGithubAccessToken = useDebounceFn((code) => {
         alert('获取code成功，请重新打开ONES Helper即可');
       }
     });
-}, 2000);
+}, 1000);
 
 export class HeaderCustomer {
   private patterns: string[] = [];
@@ -52,48 +78,24 @@ export class HeaderCustomer {
   };
 
   constructor() {
-    this.addCustomHeadersListener();
+    this.updateCustomHeadersRule();
   }
 
-  buildHeaders = (details: Browser.WebRequest.OnBeforeSendHeadersDetailsType): Headers => {
-    return this.options.headersBuilder ? this.options.headersBuilder(details) : [];
-  };
-
-  setHeadersBuilder = (headersBuilder: HeaderCustomerOptions['headersBuilder']): void => {
-    this.options.headersBuilder = headersBuilder;
-  };
-
-  // getPatterns = (): string[] => {
-  //   return this.patterns
-  // }
-
   setPatterns = (patterns: string[]): void => {
-    this.patterns = patterns;
+    this.patterns = patterns.length === 0 ? myDomains : patterns;
     this.onPatternsChange();
   };
 
-  handleRequest = (
-    details: Browser.WebRequest.OnBeforeSendHeadersDetailsType
-  ): Browser.WebRequest.BlockingResponseOrPromise => {
-    let shouldAddHeaders = false;
+  handleRequest = (details: Browser.WebRequest.OnBeforeSendHeadersDetailsType) => {
     if (details.type === 'main_frame') {
       const { url } = details;
       // GitHub授权，配置写死了http://localhost:9030/githubAuth
-      if (url.includes('http://localhost:9030/githubAuth?code=')) {
-        const urlObj = new URL(url);
-        const code = urlObj.searchParams.get('code');
+      const githubAuthStr = 'http://localhost:9030/githubAuth?code=';
+      if (url.includes(githubAuthStr)) {
+        const code = url.replace(githubAuthStr, '');
         setGithubAccessToken(code);
-      } else if (url.includes('download_export_task')) {
-        shouldAddHeaders = true;
       }
-    } else if (details.requestHeaders) {
-      shouldAddHeaders = true;
     }
-    if (shouldAddHeaders) {
-      details.requestHeaders.push(...this.buildHeaders(details));
-      details.requestHeaders.push(...this.authHeaders);
-    }
-    return { requestHeaders: details.requestHeaders };
   };
 
   handleResponseHeaders = (
@@ -138,354 +140,137 @@ export class HeaderCustomer {
     return { responseHeaders };
   };
 
-  addCustomHeadersListener = (): void => {
-    return;
-    const defaultS = [
+  updateCustomHeadersRule = (): void => {
+    const myDomains = [
       'https://dev.myones.net/*',
       'http://dev.localhost:3000/*',
       'http://dev.localhost/*',
-      'http://192.168.1.45:9001/*',
-      'http://192.168.1.210:9001/*',
       'http://localhost/*',
     ];
-    const patterns = this.patterns.length === 0 ? defaultS : this.patterns;
+    const patterns = this.patterns.length === 0 ? myDomains : this.patterns;
 
-    // const ruleId = 1;
-    //
-    // const rules = {
-    //   removeRuleIds: [ruleId],
-    //   addRules: [
-    //     {
-    //       id: ruleId,
-    //       priority: 1,
-    //       condition: {
-    //         domains: defaultS,
-    //         resourceTypes: ['main_frame', 'xmlhttprequest'],
-    //       },
-    //       action: {
-    //         type: 'modifyHeaders',
-    //         requestHeaders: [
-    //           {
-    //             header: 'X-DeclarativeNetRequest-Sample',
-    //             operation: 'set',
-    //             value: 'request',
-    //           },
-    //         ],
-    //         responseHeaders: [
-    //           {
-    //             header: 'X-DeclarativeNetRequest-Sample',
-    //             operation: 'set',
-    //             value: 'response',
-    //           },
-    //         ],
-    //       },
-    //     },
-    //   ],
-    // };
-    //
-    // chrome.declarativeNetRequest.updateDynamicRules(rules, () => {
-    //   if (browser.runtime.lastError) {
-    //     console.error(browser.runtime.lastError);
-    //   } else {
-    //     browser.declarativeNetRequest.getDynamicRules((rules) => console.log(rules));
-    //   }
-    // });
+    browser.webRequest.onBeforeSendHeaders.addListener(
+      this.handleRequest,
+      {
+        urls: patterns,
+        types: ['xmlhttprequest', 'stylesheet', 'main_frame'],
+      },
+      ['requestHeaders']
+    );
 
-    // browser.webRequest.onBeforeSendHeaders.addListener(
-    //   this.handleRequest,
-    //   {
-    //     urls: patterns,
-    //     types: ['xmlhttprequest', 'stylesheet', 'script', 'main_frame'],
-    //   },
-    //   ['blocking', 'requestHeaders']
-    // );
-    //
-    // const extraInfoSpec: Browser.WebRequest.OnHeadersReceivedOptions[] = [
-    //   'blocking',
-    //   'responseHeaders',
-    // ];
-    // if (!isFirefox) {
-    //   extraInfoSpec.push('extraHeaders');
-    // }
-    // browser.webRequest.onHeadersReceived.addListener(
-    //   this.handleResponseHeaders,
-    //   {
-    //     urls: patterns,
-    //     types: ['xmlhttprequest'],
-    //   },
-    //   extraInfoSpec
-    // );
+    const extraInfoSpec: Browser.WebRequest.OnHeadersReceivedOptions[] = ['responseHeaders'];
+    if (!isFirefox) {
+      extraInfoSpec.push('extraHeaders');
+    }
+    browser.webRequest.onHeadersReceived.addListener(
+      this.handleResponseHeaders,
+      {
+        urls: patterns,
+        types: ['xmlhttprequest'],
+      },
+      extraInfoSpec
+    );
   };
 
   removeCustomHeadersListener = (): void => {
-    // if (browser.webRequest.onBeforeSendHeaders.hasListener(this.handleRequest))
-    //   browser.webRequest.onBeforeSendHeaders.removeListener(this.handleRequest);
+    if (browser.webRequest.onBeforeSendHeaders.hasListener(this.handleRequest)) {
+      browser.webRequest.onBeforeSendHeaders.removeListener(this.handleRequest);
+    }
+  };
+
+  getModifyHeadersApiHostRule = ({ id, value, urlFilter }: ProxyParams) => {
+    return {
+      id,
+      action: {
+        type: RuleActionType.MODIFY_HEADERS,
+        requestHeaders: [
+          {
+            header: 'x-ones-api-host',
+            operation: HeaderOperation.SET,
+            value,
+          },
+        ],
+      },
+      condition: {
+        domains: myDomains,
+        urlFilter,
+        resourceTypes,
+      },
+    };
+  };
+
+  getModifyHeadersBranchRule = ({ id, value, urlFilter }: ProxyParams) => {
+    return {
+      id,
+      action: {
+        type: RuleActionType.MODIFY_HEADERS,
+        requestHeaders: [
+          {
+            header: 'x-ones-api-branch-project',
+            operation: HeaderOperation.SET,
+            value,
+          },
+          {
+            header: 'x-ones-api-branch-wiki',
+            operation: HeaderOperation.SET,
+            value,
+          },
+        ],
+      },
+      condition: {
+        domains: myDomains,
+        urlFilter,
+        resourceTypes,
+      },
+    };
   };
 
   onPatternsChange = (): void => {
-    console.log(3333);
-
-    const defaultS = [
-      'https://dev.myones.net/*',
-      'http://dev.localhost:3000/*',
-      'http://dev.localhost/*',
-      'http://192.168.1.45:9001/*',
-      'http://192.168.1.210:9001/*',
-      'http://localhost/*',
-      'https://www.bilibili.com/',
-      'https://api.bilibili.com/',
-      'api.bilibili.com',
-      'bilibili.com',
-    ];
-    const patterns = this.patterns.length === 0 ? defaultS : this.patterns;
-
-    console.log(this.buildHeaders());
-    console.log(this.authHeaders);
-
     browser.storage.local.get('customApiData').then(({ customApiData = {} as any }) => {
-      const headers: Headers = [];
-      const rules = [];
+      // const headers: Headers = [];
+      const rules: chrome.declarativeNetRequest.Rule[] = [];
       const { preset = DefaultPreset, presetOptions = DefaultPresetOptions as PresetOption[] } =
         customApiData;
       const selectedConfig = presetOptions.find((v: any) => v.value === preset).config;
       const customHOST = selectedConfig[ONES_HOST_KEY];
       if (customHOST) {
-        const value = customHOST;
-        rules.push({
+        const projectRule = this.getModifyHeadersApiHostRule({
           id: NetRequestIDMap.ProjectAPI,
-          action: {
-            type: 'modifyHeaders',
-            requestHeaders: [
-              {
-                header: 'x-ones-api-host',
-                operation: 'set',
-                value,
-              },
-            ],
-          },
-          condition: {
-            domains: defaultS,
-            urlFilter: '/api/project/',
-            resourceTypes: [
-              'main_frame',
-              'sub_frame',
-              'stylesheet',
-              'script',
-              'image',
-              'font',
-              'object',
-              'xmlhttprequest',
-              'ping',
-              'csp_report',
-              'media',
-              'websocket',
-              'webtransport',
-              'webbundle',
-              'other',
-            ],
-          },
+          value: customHOST,
+          urlFilter: ApiPath.Project,
         });
-        rules.push({
+        const wikiRule = this.getModifyHeadersApiHostRule({
           id: NetRequestIDMap.WikiAPI,
-          action: {
-            type: 'modifyHeaders',
-            requestHeaders: [
-              {
-                header: 'x-ones-api-host',
-                operation: 'set',
-                value: customHOST.replace('api/project/', 'api/wiki/'),
-              },
-            ],
-          },
-          condition: {
-            domains: defaultS,
-            urlFilter: '/api/wiki/',
-            resourceTypes: [
-              'main_frame',
-              'sub_frame',
-              'stylesheet',
-              'script',
-              'image',
-              'font',
-              'object',
-              'xmlhttprequest',
-              'ping',
-              'csp_report',
-              'media',
-              'websocket',
-              'webtransport',
-              'webbundle',
-              'other',
-            ],
-          },
+          value: customHOST.replace(ApiPath.Project, ApiPath.Wiki),
+          urlFilter: ApiPath.Wiki,
         });
+        rules.push(projectRule, wikiRule);
       } else {
         const projectBranch = selectedConfig[PROJECT_BRANCH_KEY];
         if (projectBranch) {
-          rules.push({
+          const projectRule = this.getModifyHeadersBranchRule({
             id: NetRequestIDMap.ProjectAPI,
-            action: {
-              type: 'modifyHeaders',
-              requestHeaders: [
-                {
-                  header: 'x-ones-api-branch-project',
-                  operation: 'set',
-                  value: `/project/${projectBranch}/`,
-                },
-                {
-                  header: 'x-ones-api-branch-wiki',
-                  operation: 'set',
-                  value: `/project/${projectBranch}/`,
-                },
-              ],
-            },
-            condition: {
-              domains: defaultS,
-              urlFilter: '/api/project/',
-              resourceTypes: [
-                'main_frame',
-                'sub_frame',
-                'stylesheet',
-                'script',
-                'image',
-                'font',
-                'object',
-                'xmlhttprequest',
-                'ping',
-                'csp_report',
-                'media',
-                'websocket',
-                'webtransport',
-                'webbundle',
-                'other',
-              ],
-            },
+            value: ApiPath.Project,
+            urlFilter: ApiPath.Project,
           });
-          rules.push({
+          const wikiRule = this.getModifyHeadersBranchRule({
             id: NetRequestIDMap.WikiAPI,
-            action: {
-              type: 'modifyHeaders',
-              requestHeaders: [
-                {
-                  header: 'x-ones-api-branch-project',
-                  operation: 'set',
-                  value: `/wiki/${projectBranch}/`,
-                },
-                {
-                  header: 'x-ones-api-branch-wiki',
-                  operation: 'set',
-                  value: `/wiki/${projectBranch}/`,
-                },
-              ],
-            },
-            condition: {
-              domains: defaultS,
-              urlFilter: '/api/wiki/',
-              resourceTypes: [
-                'main_frame',
-                'sub_frame',
-                'stylesheet',
-                'script',
-                'image',
-                'font',
-                'object',
-                'xmlhttprequest',
-                'ping',
-                'csp_report',
-                'media',
-                'websocket',
-                'webtransport',
-                'webbundle',
-                'other',
-              ],
-            },
+            value: ApiPath.Wiki,
+            urlFilter: ApiPath.Wiki,
           });
+          rules.push(projectRule, wikiRule);
         }
       }
 
-      console.log(rules);
+      console.log(this.authHeaders);
+
       browser.declarativeNetRequest.updateSessionRules({
         removeRuleIds: [NetRequestIDMap.WikiAPI, NetRequestIDMap.ProjectAPI],
         addRules: rules,
       });
     });
 
-    // browser.declarativeNetRequest.updateSessionRules({
-    //   removeRuleIds: [1],
-    //   addRules: [
-    //     {
-    //       id: 1,
-    //       action: {
-    //         type: 'modifyHeaders',
-    //         requestHeaders: [
-    //           {
-    //             header: 'user-agent',
-    //             operation: 'set',
-    //             value: 'ones788999',
-    //           },
-    //         ],
-    //       },
-    //       condition: {
-    //         domains: defaultS,
-    //         // urlFilter: '|https*',
-    //         // tabIds: [tab.id],
-    //         resourceTypes: [
-    //           'main_frame',
-    //           'sub_frame',
-    //           'stylesheet',
-    //           'script',
-    //           'image',
-    //           'font',
-    //           'object',
-    //           'xmlhttprequest',
-    //           'ping',
-    //           'csp_report',
-    //           'media',
-    //           'websocket',
-    //           'webtransport',
-    //           'webbundle',
-    //           'other',
-    //         ],
-    //       },
-    //     },
-    //     {
-    //       id: 2,
-    //       action: {
-    //         type: 'modifyHeaders',
-    //         requestHeaders: [
-    //           {
-    //             header: 'user-agent',
-    //             operation: 'set',
-    //             value: 'ones788999',
-    //           },
-    //         ],
-    //       },
-    //       condition: {
-    //         domains: defaultS,
-    //         // urlFilter: '|https*',
-    //         // tabIds: [tab.id],
-    //         resourceTypes: [
-    //           'main_frame',
-    //           'sub_frame',
-    //           'stylesheet',
-    //           'script',
-    //           'image',
-    //           'font',
-    //           'object',
-    //           'xmlhttprequest',
-    //           'ping',
-    //           'csp_report',
-    //           'media',
-    //           'websocket',
-    //           'webtransport',
-    //           'webbundle',
-    //           'other',
-    //         ],
-    //       },
-    //     },
-    //   ],
-    // });
-    // this.removeCustomHeadersListener();
-    // this.addCustomHeadersListener();
+    this.removeCustomHeadersListener();
+    this.updateCustomHeadersRule();
   };
 }
