da8dec643b0cdbbc6159d78f2f6f3f9196f24ca4
xlzy <1017866168@qq.com>
2022-06-10 16:31:08 +0800

    feat: 添加图标

---
+++ src/components/plus-icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <n-icon size="20" color="#0e7a0d" class="cursor-pointer">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      aria-hidden="true"
+      role="img"
+      class="iconify iconify--mdi"
+      width="32"
+      height="32"
+      preserveAspectRatio="xMidYMid meet"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="M12 20c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m0-18A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2m1 5h-2v4H7v2h4v4h2v-4h4v-2h-4V7Z"
+      ></path>
+    </svg>
+  </n-icon>
+</template>
+
+<script setup>
+import { NIcon } from 'naive-ui';
+</script>
